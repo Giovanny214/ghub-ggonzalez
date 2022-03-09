1d02b0d59664f36e019a3bf6fac034dde586d656
Giovanny214 <86082030+Giovanny214@users.noreply.github.com>
2022-03-09 12:07:40 -0800

    news

---
+++ future.js
@@ -0,0 +1,11 @@
+var job = ["Mechanic","Superhero","Programmer","Detective"];
+var city = ["Austin","Dallas","NYC","LA"];
+var spouse = ["Andrea","Kayla","Mia","Kimberly"];
+var numKids =[1,,3,2,0];
+var xx = Math.floor(Math.random() * 4)
+
+function fortune(job,city,spouse,numKids){
+document.write("You will be a " + job + " in " + city + ", and married to " + spouse + " with " + numKids + " kids.");
+}
+
+fortune (job[xx],city[xx],spouse[xx],numKids[xx])
